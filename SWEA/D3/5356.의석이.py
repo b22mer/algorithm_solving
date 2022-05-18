c78cad93f722e605fa6ebb6287a0e5f058d67e40
woncheol <gksrud316@naver.com>
2022-05-18 13:21:11 +0900

    ssafy 대비 d3 add

---
+++ SWEA/D3/5356.의석이.py
@@ -0,0 +1,26 @@
+T=int(input())
+for i in range(T):
+  testList=[]
+  max=0
+  for j in range(5):
+    testList.append(list(input()))
+    if max<len(testList[j]):
+      max=len(testList[j])
+
+  for x in range(5):
+    for y in range(max-len(testList[x])):
+      testList[x].append("*")
+
+  resultList=[]
+
+  for j in range(max):
+    for k in range(5):
+      resultList.append(testList[k][j])
+
+  resultList=[i for i in resultList if i not in "*"]
+  print("#{} {}".format(i+1,"".join(resultList)))
+
+
+
+
+  
